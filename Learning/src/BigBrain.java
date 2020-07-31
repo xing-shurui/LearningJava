378e5a8ab0ed70f8a0eb737d893c093c0b855573
Peter <peterxing7@gmail.com>
2020-07-31 12:30:43 -0500

    Convert queue to stack

--- Learning/src/BigBrain.java
+++ Learning/src/BigBrain.java
@@ -1,28 +1,30 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class BigBrain {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		
-		Scanner scanner = new Scanner(new File("student.txt"));
+	public static void main(String[] args) {
+		try {
+			Scanner scanner =new Scanner(new File("test"));
+		} catch (FileNotFoundException e) {
 			
-		List<String> students = new ArrayList<String>();
-		
-		
-		while(scanner.hasNextLine()) {
-			students.add(scanner.nextLine());
+			e.printStackTrace();
 		}
+			
+		LinkedList<String> names = new LinkedList<String>();
 		
-		for(int i = 0; i < students.size(); i++) {
-			System.out.println("name: " + students.get(i));
-		}
+		names.push("Caleb");
+		names.push("Peter");
+		names.push("Sally");
 		
-		scanner.close();
-		
+		System.out.println(names.pop());
+		System.out.println(names.pop());
+		System.out.println(names.pop());
 		
 	}
 }
